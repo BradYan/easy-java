862dd1676459b6c630e073c3142ee70185e2f6f2
BradYan <yanwenpei@aliyun.com>
2020-12-14 00:56:42 +0800

    '第一个类和对象。'

--- a01-java-language-core/src/main/java/a01/Java语言核心技术/c02/基础概念和语法/s02/小而全的类/CoreLearnBook.java
+++
@@ -1,4 +0,0 @@
-package a01.Java语言核心技术.c02.基础概念和语法.s02.小而全的类;
-
-public class CoreLearnBook {
-}
---
+++ a01-java-language-core/src/main/java/a01/Java语言核心技术/c02/基础概念和语法/s02/第一个类和对象/Book.java
@@ -0,0 +1,13 @@
+package a01.Java语言核心技术.c02.基础概念和语法.s02.第一个类和对象;
+
+ class Book{
+
+    String name; //书名
+    String author; //作者
+    int rank; //排名
+
+     public static void main(String[] args) {
+         System.out.println("ces");
+     }
+
+}
