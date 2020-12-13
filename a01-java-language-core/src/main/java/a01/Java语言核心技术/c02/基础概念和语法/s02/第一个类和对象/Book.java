9736cbc892b3010f19b28035292cdd67619a0a18
BradYan <yanwenpei@aliyun.com>
2020-12-14 01:11:32 +0800

    '第一个类和对象。'

--- a01-java-language-core/src/main/java/a01/Java语言核心技术/c02/基础概念和语法/s02/第一个类和对象/Book.java
+++ a01-java-language-core/src/main/java/a01/Java语言核心技术/c02/基础概念和语法/s02/第一个类和对象/Book.java
@@ -7,7 +7,10 @@ package a01.Java语言核心技术.c02.基础概念和语法.s02.第一个类和
     int rank; //排名
 
      public static void main(String[] args) {
-         System.out.println("ces");
+         new Book();//如果不将实例化的对象赋值，它就是孤魂野鬼，找不到归属。其它代码想用它也没办法。因为不知道它叫啥。所以要起个名字，能够引用到它。
+         Book book = new Book();
+         Book book2 = new Book();
+         Book book3 = new Book();
      }
 
 }
