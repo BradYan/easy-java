8a13106fae5c8fccaf08c5f254752972ccd86ffb
aar0fzz <aarofzz@mmm.com>
2020-12-22 18:44:52 +0800

    添加变量类型的作用域

--- a01-java-language-core/src/main/java/a01/Java语言核心技术/c02/基础概念和语法/s04/变量类型/VariableType.java
+++ a01-java-language-core/src/main/java/a01/Java语言核心技术/c02/基础概念和语法/s04/变量类型/VariableType.java
@@ -2,47 +2,36 @@ package a01.Java语言核心技术.c02.基础概念和语法.s04.变量类型;
 
 public class VariableType {
 
-    String name;    //新建对象的时候，默认为null
-    int totalGrade; //新建对象的时候，默认为0
-    double sellPrice; //新建对象的时候，默认为0.0
-
     public static void main(String[] args) {
-        //变量申明
-        System.out.println("------变量申明-------------------------------------------");
+        int lVa = 10;
+        System.out.println("方法局部变量, lVa =" + lVa);
 
-        String bookname;    // String 类型的变量
-        boolean isPopular;    // boolean 类型的变量
-        int totalWords;    // int 类型的变量
+        System.out.println("方法参数局部变量: args ="+ args);
 
-        int a, b, c;         // 声明三个int型整数：a、 b、c
-        System.out.println("-------------------------------------------------");
-        System.out.println("------直接申明-------------------------------------------");
-        //直接申明
-        String author = "曹雪芹";     // 直接赋值
-        double pi = 3.1415926; // 声明了双精度浮点型变量 pi
-        int d = 3, e = 4, f = 5; // 声明三个整数并赋予初值
+        {
+            int lVb = 5;
+            System.out.println("代码块局部变量, lVb = " + lVb);
+            System.out.println("代码块中可以访问到 lVa =" + lVa);
 
+        }
 
-        System.out.println("-------------------------------------------------");
-        System.out.println("------先申明，后赋值。-------------------------------------------");
-        //先申明，后赋值。
-        String bookName;    // 先声明
-        bookName ="红楼梦";    // 后赋值
+        {
+            int lVc = 3;
+            System.out.println("代码块局部变量, lVc = " + lVc);
+            System.out.println("代码块中可以访问到 lVa =" + lVa);
+            System.out.println("代码块中可以访问到 args =" + args);
+//            System.out.println("代码块中不可以访问到 lVb ." + lVb); //取消注释,查看报错效果.
 
-        String readerName,grade, comment;//读者，评分，评价    // 声明多个变量
-        readerName = "布拉德";
-        grade = "经典巨著";
-        comment = "伟大的作品，禁得起时间的考验";
+        }
+//            System.out.println("这里也不可以访问到 lVb,lVc. lVb=" + lVb+",lVc="+lVc); //取消注释,查看报错效果.
+    }
 
+    public static void testFunctionA(){
+        int a = 1;
+        System.out.println("方法局部变量, a = " + a);
+    }
 
-        System.out.println("-------------------------------------------------");
-        System.out.println("------初始化值。-------------------------------------------");
-        VariableType variableType = new VariableType();
-        System.out.println("String初始化值为:"+variableType.name);
-        System.out.println("int初始化值为:"+variableType.totalGrade);
-        System.out.println("double初始化值为:"+variableType.sellPrice);
-        int y;
-        System.out.println("-----在方法中，未初始化，会出错。开发工具会提示该错误。-----------");
-//        System.out.println(y); //取消该注释，会看到红色的提示。
+    public static void testFunctionB(double input){
+        System.out.println("方法参数局部变量, input = " + input);
     }
 }
